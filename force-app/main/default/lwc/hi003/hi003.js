377a4992d184842c28f9759c5673756f964b33b8
Ciro Cesar Maciel <ciro.maciel@rili.be>
2021-11-29 18:47:56 -0300

    feat: getAccount

--- force-app/main/default/lwc/hi003/hi003.js
+++ force-app/main/default/lwc/hi003/hi003.js
@@ -1,5 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from "lwc";
+
+import getAccount from "@salesforce/apex/c/hi001.getAccount";
+
+
+const columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Type', fieldName: 'Type'}
+];
+
 
 export default class Hi003 extends LightningElement {
+
+
+    @track
+    _accounts = [];
+
+
+
+    connectedCallback() {
+
+        this.retrieveAccount();
+        
+    }
+
+
+    async retrieveAccount() {
+        
+        this._accounts = await getAccount();
+    }
     
 }
